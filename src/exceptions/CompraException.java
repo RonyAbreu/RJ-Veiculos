3ac0ad7b5fdcd454e973a493f9b9892377eb2a4f
RonyAbreu <ronyelison78@gmail.com>
2024-01-17 14:41:03 -0300

    criando classe para persistir os dados em arquivos

--- src/exceptions/CompraException.java
+++ src/exceptions/CompraException.java
@@ -2,7 +2,7 @@ package exceptions;
 
 public class CompraException extends RuntimeException{
     public CompraException(){
-        super("Nenhuma compra cadastrada, ou conta inexistente! ");
+        super("Nenhuma compra cadastrada, ou conta inexistente!");
     }
 
     public CompraException(String msg){
